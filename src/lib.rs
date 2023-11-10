c2c27e56c1b752890749c4e902e8d78db0762118
aoikurokawa <ht.so.ik0102@gmail.com>
2023-11-11 07:12:42 +0900

    feat: add new instruction for adding movie

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,39 @@
 pub mod instruction;
 
+use instruction::MovieInstruction;
 use solana_program::{
-    account_info::AccountInfo, entrypoint, entrypoint::ProgramResult, pubkey::Pubkey,
+    account_info::AccountInfo, entrypoint, entrypoint::ProgramResult, msg, pubkey::Pubkey,
 };
 
 entrypoint!(process_instruction);
 
 pub fn process_instruction(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    let instruction = MovieInstruction::unpack(instruction_data)?;
+
+    match instruction {
+        MovieInstruction::AddMovieReview {
+            title,
+            rating,
+            description,
+        } => add_movie_review(program_id, accounts, title, rating, description),
+    }
+}
+
+pub fn add_movie_review(
     _program_id: &Pubkey,
     _accounts: &[AccountInfo],
-    _instruction_data: &[u8],
+    title: String,
+    rating: u8,
+    description: String,
 ) -> ProgramResult {
+    msg!("Adding movie review...");
+    msg!("Title: {}", title);
+    msg!("Rating: {}", rating);
+    msg!("Description: {}", description);
+
     Ok(())
 }
