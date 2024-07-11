f5b7849b998c74f5e26653821af5ec0dd183fb68
aoikurokawa <ht.so.ik0102@gmail.com>
2024-07-12 08:22:17 +0900

    feat: update

--- programs/zone/src/lib.rs
+++ programs/zone/src/lib.rs
@@ -1,7 +1,6 @@
 use anchor_lang::prelude::*;
-use anchor_spl::token::{Mint, Token, TokenAccount};
 
-declare_id!("FuF87VDgECo9tETuve2obCmt38r7EZM2HC34D8ePo6fz");
+declare_id!("7UyLYeoNwWeh3LgMUnWFKPc1Ebwr8Afzsz8hVjgavoRa");
 
 pub mod constants {
     pub const VAULT_SEED: &[u8] = b"vault";
@@ -122,26 +121,7 @@ pub mod zone {
 }
 
 #[derive(Accounts)]
-pub struct Initialize<'info> {
-    #[account(
-        init_if_needed,
-        seeds = [constants::VAULT_SEED],
-        bump,
-        payer = signer,
-        token::mint = mint,
-        token::authority = token_vault_account,
-    )]
-    pub token_vault_account: Account<'info, TokenAccount>,
-
-    #[account(mut)]
-    pub signer: Signer<'info>,
-
-    pub mint: Account<'info, Mint>,
-
-    pub token_program: Program<'info, Token>,
-
-    pub system_program: Program<'info, System>,
-}
+pub struct Initialize {}
 
 #[derive(Accounts)]
 pub struct InitializeMarket<'info> {
